c632f1e925a9fcc0835897366d263a65f5dd36e2
Alexander Zavyalov <whmordred@gmail.com>
2021-04-17 18:50:30 +0800

    Fixed indentation

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import init from './init.js';
 import SvelteApp from '../svelte/App.svelte';
 
 const svelteApp = new SvelteApp({
-	target: document.body
+    target: document.body
 });
 
 export default svelteApp;
--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -1,25 +1,25 @@
 <script>
-	import { onMount } from 'svelte';
+    import { onMount } from 'svelte';
 
-	import '../sass/app.scss';
-	import { PageRenderer } from '../../../sdk/v0.4.x/svelte-components.js';
-	import { Navigator } from '../../../sdk/v0.4.x/sdk.js';
+    import '../sass/app.scss';
+    import { PageRenderer } from '../../../sdk/v0.4.x/svelte-components.js';
+    import { Navigator } from '../../../sdk/v0.4.x/sdk.js';
 
-	import pages from '../js/pages.js';
+    import pages from '../js/pages.js';
 
-	onMount(() => {
-		Navigator.setPages(pages);
-		Navigator.goTo('home');
-	});
+    onMount(() => {
+        Navigator.setPages(pages);
+        Navigator.goTo('home');
+    });
 </script>
 
 <style lang="scss">
-	main {
-		flex: 1;
-		text-align: center;
-	}
+    main {
+        flex: 1;
+        text-align: center;
+    }
 </style>
 
 <main>
-	<PageRenderer/>
+    <PageRenderer/>
 </main>
